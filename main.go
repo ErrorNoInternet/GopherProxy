e3f851060f262d6889cdd2c0b4da9dd836440f79
ErrorNoInternet <error.nointernet@gmail.com>
2022-12-08 22:38:53 +0800

    Add more security

--- main.go
+++ main.go
@@ -10,12 +10,31 @@ import (
 	"net/url"
 	"os"
 	"strings"
+	"sync"
 	"time"
 )
 
 var (
-	clientAddresses map[string]string
+	mapMutex        *sync.Mutex
+	ratelimits      map[string]int
+	clientAddresses map[string]*url.URL
 )
+
+func getCounter(ip string) int {
+	mapMutex.Lock()
+	counter, ok := ratelimits[ip]
+	mapMutex.Unlock()
+	if !ok {
+		return 0
+	}
+	return counter
+}
+
+func setCounter(ip string, counter int) {
+	mapMutex.Lock()
+	ratelimits[ip] = counter
+	mapMutex.Unlock()
+}
 
 func handleClient(writer http.ResponseWriter, request *http.Request) {
 	if request.Method == "GET" {
@@ -26,15 +45,20 @@ func handleClient(writer http.ResponseWriter, request *http.Request) {
 			fmt.Fprintf(writer, err.Error())
 			return
 		}
-		ip := strings.Split(request.RemoteAddr, ":")[0]
-		parsedUrl, err := url.QueryUnescape(strings.TrimPrefix(string(rawUrl), "url="))
+		decodedUrl, err := url.QueryUnescape(strings.TrimPrefix(string(rawUrl), "url="))
 		if err != nil {
 			fmt.Fprintf(writer, err.Error())
 			return
 		}
-		url := strings.TrimSuffix(parsedUrl, "/")
-		clientAddresses[ip] = url
-		fmt.Printf("%v is now assigned to %v\n", ip, url)
+		parsedUrl, err := url.Parse(strings.TrimSuffix(decodedUrl, "/"))
+		if err != nil {
+			fmt.Fprintf(writer, err.Error())
+			return
+		}
+
+		ip := strings.Split(request.RemoteAddr, ":")[0]
+		clientAddresses[ip] = parsedUrl
+		fmt.Printf("%v is now assigned to %v\n", ip, parsedUrl)
 		fmt.Fprintf(writer, "<!DOCTYPE html><meta http-equiv=\"Refresh\" content=\"0; url='/'\"/>")
 	}
 }
@@ -43,18 +67,35 @@ func proxyRequest(writer http.ResponseWriter, request *http.Request) {
 	session := rand.Intn(int(math.Pow(2, 31)))
 
 	ip := strings.Split(request.RemoteAddr, ":")[0]
-	url, ok := clientAddresses[ip]
+	for getCounter(ip) >= 25 {
+		time.Sleep(1 * time.Second)
+	}
+	setCounter(ip, getCounter(ip)+1)
+	parsedUrl, ok := clientAddresses[ip]
 	if !ok {
 		fmt.Fprintf(writer, "<!DOCTYPE html><meta http-equiv=\"Refresh\" content=\"0; url='/__gopherproxy__'\"/>")
 		return
 	}
-	url = url + request.URL.Path
+	url := parsedUrl.String() + request.URL.Path
+	if len(url) > 6969 {
+		fmt.Fprintf(writer, "Don't try to exploit me!")
+		return
+	}
 	fmt.Printf("[%v] %v is sending a %v request to %v...\n", session, ip, request.Method, url)
 
 	client := http.Client{}
 	newRequest, _ := http.NewRequest(request.Method, url, nil)
+	newRequest.Header.Add("Handled-By", "GopherProxy")
 	for key, value := range request.Header {
-		newRequest.Header.Add(key, value[0])
+		key = strings.ToLower(key)
+		if key == "origin" || key == "referer" {
+			continue
+		}
+		if key == "host" {
+			newRequest.Header.Add(key, parsedUrl.Host)
+		} else {
+			newRequest.Header.Add(key, value[0])
+		}
 	}
 	response, err := client.Do(newRequest)
 	if err != nil {
@@ -64,6 +105,10 @@ func proxyRequest(writer http.ResponseWriter, request *http.Request) {
 
 	for key, value := range response.Header {
 		key = strings.ToLower(key)
+		if key == "handled-by" && value[0] == "GopherProxy" {
+			fmt.Fprintf(writer, "Don't try to exploit me!")
+			return
+		}
 		if key == "content-length" || key == "set-cookie" {
 			continue
 		}
@@ -98,7 +143,10 @@ func proxyRequest(writer http.ResponseWriter, request *http.Request) {
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	clientAddresses = make(map[string]string)
+	mapMutex = &sync.Mutex{}
+	ratelimits = make(map[string]int)
+	clientAddresses = make(map[string]*url.URL)
+	go cleanup()
 
 	http.HandleFunc("/__gopherproxy__", handleClient)
 	http.HandleFunc("/", proxyRequest)
@@ -110,5 +158,20 @@ func main() {
 	fmt.Printf("Starting server on port %v...\n", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		fmt.Printf("Unable to bind address: %v\n", err)
+	}
+}
+
+func cleanup() {
+	for {
+		time.Sleep(1 * time.Second)
+		mapMutex.Lock()
+		for ip, ratelimit := range ratelimits {
+			if ratelimit == 0 {
+				delete(ratelimits, ip)
+			} else {
+				ratelimits[ip] = ratelimit - 1
+			}
+		}
+		mapMutex.Unlock()
 	}
 }
